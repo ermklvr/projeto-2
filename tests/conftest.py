9f354a3040c638800aae1294c234841bebc96226
Gustavo <mechamoguto@gmail.com>
2026-09-21 22:16:03 -0300

    test: separate test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,40 @@
+import os
+
 import pytest
+from dotenv import load_dotenv
 
-from tests.test_products import client
+load_dotenv()
+
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+
+if not TEST_DATABASE_URL:
+    raise RuntimeError("TEST_DATABASE_URL não configurada")
+
+os.environ["DATABASE_URL"] = TEST_DATABASE_URL
+
+from app.database import Base, engine
 
 
-#---------------------FIXTURE-----------------------
+@pytest.fixture(scope="session", autouse=True)
+def setup_test_database():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+
+
 @pytest.fixture
 def category_id():
+    from tests.test_products import client
+
     response = client.post(
         "/categories",
-        json = {
+        json={
             "name": "Categoria Teste"
         }
     )
-    
+
     assert response.status_code == 200
 
     return response.json()["id"]
